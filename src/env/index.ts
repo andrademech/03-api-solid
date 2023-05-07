99e460128fd2d6873a9ba6bcce452c4a3e26003b
Herberth Andrade <dev.herberth@gmail.com>
2023-05-07 15:27:16 -0300

    develop: env variables included and eslint config

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+
+// validate env variables
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.log('Invalid env variables!', _env.error.format())
+
+  // doenst permit the server to start
+  throw new Error('Invalid env variables!')
+}
+
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app"
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
