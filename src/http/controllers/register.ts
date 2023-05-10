1ccf892c4fe6dde44fae05a441f63bb44b35cd85
Herberth Andrade <dev.herberth@gmail.com>
2023-05-10 20:30:14 -0300

    develop: added register controller, created hash method, dependecy inversion principle applied

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,75 @@
+import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/user-repository'
+// import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+/**
+ * SOLID Principles
+ * S - Single Responsibility Principle
+ * O - Open Closed Principle
+ * L - Liskov Substitution Principle
+ * I - Interface Segregation Principle
+ * D - Dependency Inversion Principle <-- This one
+ */
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('User already exists')
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository()
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
+
+/**
+ * The code below is the same as the code above, but it's not using the SOLID Principles
+ * It's tottaly dependent of the PrismaUsersRepository
+ */
+// export async function registerUseCase({
+//   name,
+//   email,
+//   password,
+// }: RegisterUseCaseRequest) {
+//   const password_hash = await hash(password, 6)
+
+//   const userWithSameEmail = await prisma.user.findUnique({
+//     where: {
+//       email,
+//     },
+//   })
+
+//   if (userWithSameEmail) {
+//     throw new Error('User already exists')
+//   }
+
+//   const prismaUsersRepository = new PrismaUsersRepository()
+
+//   await prismaUsersRepository.create({
+//     name,
+//     email,
+//     password_hash,
+//   })
+// }
