6858eea553eef9bf9ea12e4629f2d49c5dfa99b1
Herberth Andrade <dev.herberth@gmail.com>
2023-05-12 21:53:42 -0300

    develop: setting first one unit test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,45 @@
-import { expect, test } from 'vitest'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('check if it works', () => {
-  expect(1).toBe(1)
+// Unit Test
+// 1. Teste unitário é um teste que testa uma unidade de código
+// 2. Uma unidade de código é uma função, um método, uma classe, um componente, etc
+// 3. Um teste unitário não deve depender de nada externo
+// 4. Um teste unitário deve ser rápido
+// 5. Um teste unitário deve ser isolado e nunca bater em um banco de dados, API, etc
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example3.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+
+    console.log(user.password_hash)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,6 +2,7 @@ import { UsersRepository } from '@/repositories/user-repository'
 // import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
+import type { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -18,10 +19,18 @@ interface RegisterUseCaseRequest {
  * D - Dependency Inversion Principle <-- This one
  */
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -30,11 +39,15 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
 
