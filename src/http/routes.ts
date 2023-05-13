5d561c4bdd2c82367d9a1a9086b87ad56012bf7f
Herberth Andrade <dev.herberth@gmail.com>
2023-05-13 10:46:44 -0300

    develop: implementing factory pattern in register and authenticate use case

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
+import { register } from './controllers/register.controller'
+import { authenticate } from './controllers/authenticate.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
