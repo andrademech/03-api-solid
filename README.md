f92f2692b9b64c57e2b60bb01afa2e65efd63eff
Herberth Andrade <dev.herberth@gmail.com>
2023-05-23 19:57:59 -0300

    develop: implementing prisma repositories

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ RNs (Regras de negócio)
  - [x] O usuário não deve poder se cadastrar com um e-mail duplicado;
  - [x] O usuário não pode fazer 2 check-ins no mesmo dia;
  - [x] O usuário não pode fazer check-in se não estiver perto (100m) da academia;
- - [ ] O check-in só pode ser validada até 20 minutos após ser criado;
+ - [x] O check-in só pode ser validada até 20 minutos após ser criado;
  - [ ] O check-in só pode ser validado por administradores;
  - [ ] A academia só pode ser cadastrada por administradores;
 
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../user-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
@@ -17,20 +27,6 @@ export class PrismaUsersRepository implements UsersRepository {
         email,
       },
     })
-
-    return user
-  }
-
-  async findById(id: string) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-      },
-    })
-
-    if (!user) {
-      return null
-    }
 
     return user
   }
