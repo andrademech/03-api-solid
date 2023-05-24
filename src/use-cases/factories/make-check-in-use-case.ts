3213408e3712b8e43f022e83fd18535b6587728c
Herberth Andrade <dev.herberth@gmail.com>
2023-05-24 14:12:42 -0300

    develop: creating factories use case

---
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -0,0 +1,11 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+import { CheckInUseCase } from '../check-in'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeCheckInUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const gymsRepository = new PrismaGymsRepository()
+  const useCase = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+  return useCase
+}
