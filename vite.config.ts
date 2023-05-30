795c68b8791cb2958e2ccff1a982f75f25b31ef2
Herberth Andrade <dev.herberth@gmail.com>
2023-05-29 22:09:34 -0300

    develop: creating local test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+  },
 })
