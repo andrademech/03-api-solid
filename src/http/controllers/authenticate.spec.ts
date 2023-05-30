ccaf9814e041e6f76bc03794c61523ec83a60ceb
Herberth Andrade <dev.herberth@gmail.com>
2023-05-29 22:52:50 -0300

    develop: test e2e to authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { expect, it, describe, beforeAll, afterAll } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    // create user
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndow@example.com',
+      password: '123456',
+    })
+
+    // authenticate
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndow@example.com',
+      password: '123456',
+    })
+
+    // expect
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
